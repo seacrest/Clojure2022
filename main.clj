3f9ddcccc2f4ce4a7b6f411f0e4f2e87b34e5e6c
Elvis Chan <ekc911@yahoo.com>
2022-12-17 19:04:45 +0000

    Initial commit

---
+++ main.clj
@@ -0,0 +1,14 @@
+(println "Hello World!")
+
+(println (macroexpand '(when boolean-expression
+                expression1
+                expression2
+               ))
+)
+                      
+                         
+(defmacro infix
+  "Use this macro when you pine for the notation of your childhood"
+  [infixed]
+  (list (second infixed) (first infixed) (last infixed)))
+
